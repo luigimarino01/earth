089f92093f626839150d613fbf22a5cf65246fa8
mcoder01 <manu01141@gmail.com>
2025-06-10 20:24:50 +0200

    Added code comments

--- scripts/converter.py
+++ scripts/converter.py
@@ -13,14 +13,17 @@ INPUT_DIR = Path("netcdf")
 OUTPUT_DIR = Path("png")
 
 def float_to_uint16(value, min, max):
+    # Converts a 32-bit floating point value into a 16-bit unsigned integer
     return np.uint16(np.around((value-min)/(max-min)*65535))
 
 def encode(val1, min1, max1, val2, min2, max2):
+    # Encodes two 32-bit floating point values into RGBA channels
     z1 = float_to_uint16(val1, min1, max1)
     z2 = float_to_uint16(val2, min2, max2)
     return [z1 >> 8, z1 & 0xFF, z2 >> 8, z2 & 0xFF]
 
 def load_data(input_dir, filename):
+    # Loads the geographical coordinates and the U and V components from the given NetCDF file
     ds = xr.open_dataset(input_dir.joinpath(filename + ".nc.nc4"))
     lats = ds[VAR_LAT].values
     lons = ds[VAR_LON].values
@@ -29,7 +32,10 @@ def load_data(input_dir, filename):
     return lats, lons, u, v
 
 def netcdf_to_png(input_dir, filename, output_dir):
+    # Converts the given NetCDF file to two PNG files, by encoding data into RGBA channels
     lats, lons, u, v = load_data(input_dir, filename)
+
+    # Encode the extracted data
     coords_img_data = np.zeros((u.shape[0], u.shape[1], 4), dtype=np.uint8)
     uv_img_data = np.zeros((u.shape[0], u.shape[1], 4), dtype=np.uint8)
     for i in range(u.shape[0]):
@@ -37,18 +43,21 @@ def netcdf_to_png(input_dir, filename, output_dir):
             coords_img_data[i, j] = encode(lats[i], -90, 90, lons[j], -180, 180)
             uv_img_data[i, j] = encode(u[i, j], -50, 50, v[i, j], -50, 50)
 
+    # Store the output images
     coords_file = output_dir.joinpath(filename + ".coords.png")
     Image.fromarray(coords_img_data, mode="RGBA").save(coords_file)
     uv_file = output_dir.joinpath(filename + ".uv.png")
     Image.fromarray(uv_img_data, mode="RGBA").save(uv_file)
 
 def serialize(data):
+    # Serialize one or more float values into their binary representations
     data = data.flatten()
     return struct.pack("f"*len(data), *data)
 
 def netcdf_to_bin(input_dir, filename, output_dir):
+    """ Extracts wind information from the given NetCDF file 
+        and stores them into a binary file """
     lats, lons, u, v = load_data(input_dir, filename)
-
     with open(output_dir.joinpath(filename + ".bin"), "wb") as f:
         f.write(struct.pack("II", u.shape[0], u.shape[1]))
         f.write(serialize(lats))
--- scripts/visualizer.py
+++ scripts/visualizer.py
@@ -4,10 +4,12 @@ import numpy as np
 from pathlib import Path
 import struct
 
+# File used by earth for visualization
 OUTPUT_FILE = Path.cwd().parent \
     .joinpath("public", "data", "weather", "current", "current-wind-surface-level-gfs-1.0.json")
 
 def param_to_json(data, nx, ny, lo1, la1, lo2, la2):
+    # Returns the JSON representation compatible with earth of the given data
     return {
             "header": {
                 "nx": nx,
@@ -26,6 +28,7 @@ def param_to_json(data, nx, ny, lo1, la1, lo2, la2):
         }
 
 def to_earth_json(lats, lons, u, v):
+    # Converts coordinates and wind information to JSON for visualizing them on earth
     lo1 = float(np.min(lons))
     lo2 = float(np.max(lons))
     la1 = float(np.max(lats))
@@ -45,14 +48,17 @@ def to_earth_json(lats, lons, u, v):
             f)
         
 def uint16_to_float(value, min, max):
+    # Converts a 16-bit unsigned integer to a floating point value
     return float(value/65535.0*(max-min)+min)
 
 def decode(rgba, min1, max1, min2, max2):
+    # Decodes RGBA channels and returns the original two values
     val1 = uint16_to_float(np.uint16(rgba[0]) << 8 | rgba[1], min1, max1)
     val2 = uint16_to_float(np.uint16(rgba[2]) << 8 | rgba[3], min2, max2)
     return val1, val2
 
 def extract_data_from_png(png_file, file_type):
+    # Extracts and returns data encoded into the given PNG file
     img = Image.open(png_file)
     pixels = img.load()
 
@@ -67,23 +73,30 @@ def extract_data_from_png(png_file, file_type):
     return x, y
 
 def png_to_earth_json(input_dir, filename):
+    # Visualize on earth the given file, by extracting information from the corresponding PNGs
     lats, lons = extract_data_from_png(input_dir.joinpath(filename + ".coords.png"), "coords")
     u, v = extract_data_from_png(input_dir.joinpath(filename + ".uv.png"), "wind")
     to_earth_json(lats, lons, u, v)
 
 def deserialize(data, len, start, value_size=4):
+    # Deserializes one or more floating point values
     end = start+len*value_size
     return np.array(struct.unpack("f"*len, data[start:end]), dtype=np.float32), end
     
 def extract_data_from_bin(filepath):
+    # Extracts coordinates and wind information from the given binary file
     with open(filepath, "rb") as f:
         data = f.read()
 
+    # Gets the dimensions of U and V matrices
     ny = struct.unpack("I", data[0:4])[0]
     nx = struct.unpack("I", data[4:8])[0]
 
+    # Extract geographical coordinates
     lats, offset = deserialize(data, ny, 8)
     lons, offset = deserialize(data, nx, offset)
+
+    # Extract wind U and V components
     u, offset = deserialize(data, ny*nx, offset)
     v, offset = deserialize(data, ny*nx, offset)
 
@@ -92,5 +105,6 @@ def extract_data_from_bin(filepath):
     return lats, lons, u, v
 
 def bin_to_earth_json(input_dir, filename):
+    # Visualize on earth the given binary file
     lats, lons, u, v = extract_data_from_bin(input_dir.joinpath(filename + ".bin"))
     to_earth_json(lats, lons, u, v)
